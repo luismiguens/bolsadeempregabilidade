ffd542ef9216b4c46e82e38ea787aa74ef2b0de9
Luis Miguens <luis.t.miguens@gmail.com>
2019-01-08 17:04:48 -0100

    contacto update

--- src/AppBundle/Form/JobType.php
+++ src/AppBundle/Form/JobType.php
@@ -8,28 +8,76 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+class JobType extends AbstractType {
 
-class JobType extends AbstractType
-{
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('title', TextType::class, ['label'=>"Titulo"] )
-                ->add('description', TextareaType::class, ['label'=>"Descrição"])
-                ->add('area', TextType::class, ['label'=>"Área"] )
-                ->add('location', TextType::class, ['label'=>"Localidade"] )
-                ->add('openings', TextType::class, ['label'=>"Vagas"] )
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('title', TextType::class, ['label' => "Titulo"])
+                ->add('description', TextareaType::class, ['label' => "Descrição"])
+                ->add('area', \Symfony\Component\Form\Extension\Core\Type\ChoiceType::class, array(
+                    'choices' => array(
+                        'Administrativos	' => 'Administrativos',
+                        'Agência de Viagens' => '	Agência de Viagens',
+                        'Animação e Desporto' => '	Animação e Desporto',
+                        'Atenção ao Cliente' => '	Atenção ao Cliente',
+                        'Aviação / Aeroporto' => '	Aviação / Aeroporto',
+                        'Comercial	' => 'Comercial',
+                        'Compras' => '	Compras',
+                        'Consultoria e Formação	' => 'Consultoria e Formação',
+                        'Cozinha' => '	Cozinha',
+                        'Cruzeiros	' => 'Cruzeiros',
+                        'Direcção	' => 'Direcção',
+                        'Eventos e Grupos' => '	Eventos e Grupos',
+                        'Guia Intérprete	' => 'Guia Intérprete',
+                        'Manutenção	' => 'Manutenção',
+                        'Marketing e Relações Públicas	' => 'Marketing e Relações Públicas',
+                        'Outra	' => 'Outra',
+                        'Qualidade' => '	Qualidade',
+                        'Quartos e Andares' => '	Quartos e Andares',
+                        'Recepção' => '	Recepção',
+                        'Recursos Humanos' => '	Recursos Humanos',
+                        'Reservas' => '	Reservas',
+                        'Sala e Bar' => '	Sala e Bar',
+                        'SPA e Beleza' => '	SPA e Beleza',
+                    ), 'label' => "Área"))
+                ->add('location', \Symfony\Component\Form\Extension\Core\Type\ChoiceType::class, array(
+                    'choices' => array(
+                        'Lisboa' => 'Lisboa',
+                        'Porto' => 'Porto',
+                        'Açores' => 'Açores',
+                        'Aveiro' => 'Aveiro',
+                        'Beja' => 'Beja',
+                        'Braga' => 'Braga',
+                        'Bragança' => 'Bragança',
+                        'Castelo Branco' => 'Castelo Branco',
+                        'Coimbra' => 'Coimbra',
+                        'Évora' => 'Évora',
+                        'Faro' => 'Faro',
+                        'Guarda' => 'Guarda',
+                        'Leiria' => 'Leiria',
+                        'Madeira' => 'Madeira',
+                        'Portalegre' => 'Portalegre',
+                        'Santarém' => 'Santarém',
+                        'Setúbal' => 'Setúbal',
+                        'Viana do Castelo' => 'Viana do Castelo',
+                        'Vila Real' => 'Vila Real',
+                        'Viseu' => 'Viseu',
+                        'Todas a regiões' => 'Todas a regiões',
+                        'Anuncio para o estrangeiro' => 'Anuncio para o estrangeiro',
+                    ), 'label' => "Localidade"))
+                ->add('openings', TextType::class, ['label' => "Vagas"])
                 //->add('createdAt')
-                ->add('business', null, ['label'=>"Empresa"] )
+                ->add('business', null, ['label' => "Empresa"])
 //                ->add('users', null, ['label'=>"Participantes"])
-            ;
-    }/**
+        ;
+    }
+
+    /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\Job'
         ));
@@ -38,10 +86,8 @@ class JobType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return 'appbundle_job';
     }
-
 
 }
